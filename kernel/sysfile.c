6dd69aba56384fc054fe8f2c3401eebe948eb93b
wanglirun <changyusong123@gmail.com>
2025-09-13 18:46:57 +0200

    lab-fs done

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -94,6 +94,7 @@ sys_write(void)
   return filewrite(f, p, n);
 }
 
+
 uint64
 sys_close(void)
 {
@@ -169,6 +170,10 @@ bad:
   return -1;
 }
 
+
+
+
+
 // Is the directory dp empty except for "." and ".." ?
 static int
 isdirempty(struct inode *dp)
@@ -242,7 +247,7 @@ bad:
   return -1;
 }
 
-static struct inode*
+struct inode*
 create(char *path, short type, short major, short minor)
 {
   struct inode *ip, *dp;
@@ -301,6 +306,8 @@ create(char *path, short type, short major, short minor)
   return 0;
 }
 
+
+
 uint64
 sys_open(void)
 {
@@ -334,8 +341,42 @@ sys_open(void)
       return -1;
     }
   }
+//lab fs.2
+  if(ip->type==T_SYMLINK&&((omode&O_NOFOLLOW)==0)){
+    int cycle=0;
+    char target[MAXPATH];
+    while(ip->type==T_SYMLINK)
+    {
+      if(cycle==0) cycle=ip->inum;
+      else if(cycle==ip->inum){
+        iunlockput(ip);
+        end_op();
+        printf("has cycle\n");
+        return -1;
+      }
+      if(readi(ip,0,(uint64)target,0,MAXPATH)!=MAXPATH){
+        iunlockput(ip);
+        end_op();
+        printf("readi failed\n");
+        return -1;        
+      }
+      iunlockput(ip);
+      if((ip = namei(target)) == 0){
+        end_op();
+        printf("No such file\n");
+        return -1; 
+      }
+      ilock(ip);
+    }
+    if(ip->type == T_DIR && omode != O_RDONLY){
+      iunlockput(ip);
+      end_op();
+      printf("try to write a directory");
+      return -1;
+    }
 
-  if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
+  }
+    if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
     iunlockput(ip);
     end_op();
     return -1;
@@ -501,5 +542,31 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+
+uint64
+sys_symlink(void){
+  char path[MAXPATH];
+  char target[MAXPATH];
+  if(argstr(0,target,MAXPATH)<0||argstr(1,path,MAXPATH)<0){
+    return -1;
+  }
+  begin_op();
+  struct inode* ip;
+  if((ip=create(path,T_SYMLINK,0,0))==0){
+     end_op();
+    return -1;
+  }
+  //ilock(ip);
+  if(writei(ip,0,(uint64)target,0,MAXPATH)!=MAXPATH){
+     iunlockput(ip);  
+     end_op();
+    return -1;
+  }
+  iupdate(ip);
+  iunlockput(ip);  
+  end_op();
   return 0;
 }
